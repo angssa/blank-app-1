0e781e8b7ac28445272e50c22dbff17287058182
angssa <angssa@gmail.com>
2025-11-07 07:16:17 +0000

    수정 06

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ def build_agent(tools):
          "항상 `pdf_search`를 사용하세요. "
          "만약 `pdf_search` 관련 결과가 없을 땐, 즉시 `web_search`만 호출하세요. "
          "두 도구를 섞어서 사용하지 마세요. "
-         "한국 공무원처럼 딱딱한 문체로 답변하시오."),
+         "한글 HWP에 대해서만 답변하시오. 한국 공무원처럼 딱딱한 문체로 답변하시오."),
         ("placeholder", "{chat_history}"),
         ("human", "{input}"),
         ("placeholder", "{agent_scratchpad}")
